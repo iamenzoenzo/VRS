d548b4e4fe310d7c4ddcee155208c5762bb3b107
jcmanhulad <57318349+jcmanhulad@users.noreply.github.com>
2020-04-13 13:35:10 +0800

    updates

--- application/views/clients/view.php
+++ application/views/clients/view.php
@@ -1,6 +1,5 @@
 <title><?= $title ;?></title>
 
-
 <div class="row">
   <div class="col">
     <h2><?= $title ;?></h2>
@@ -12,7 +11,7 @@
 
 <div class="card">
   <div class="card-header">
-    <?php echo $clients['name']; ?> Information
+    <h5><?php echo $clients['name']; ?> Information</h5>
   </div>
   <div class="card-body">
       <div class="row">
@@ -43,15 +42,17 @@
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
   </ol>
   <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img style="width:100%;height:100%;object-fit: contain;" class="d-block" src="<?php echo base_url()?>assets/images/client_images/1586743247-Capture.png" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img style="width:100%;height:100%;object-fit: contain;" class="d-block" src="<?php echo base_url()?>assets/images/client_images/no-image.jpg" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img style="width:100%;height:100%;object-fit: contain;" class="d-block" src="<?php echo base_url()?>assets/images/client_images/1586743247-Manhulad-Jeffry-CMSC208-Assignment2.jpg" alt="Third slide">
-    </div>
+    <?php
+    $counter=0;
+    foreach ($images as $image) {
+      $counter++;
+      echo '
+      <div class="carousel-item '.($counter===1 ? 'active' : ' ' ).'">
+        <img style="width:100%;height:100%;object-fit: contain;" class="d-block" src="'.base_url().'assets/images/client_images/'.$image['file_name'].'">
+      </div>
+      ';
+    }
+    ?>
   </div>
   <a class="carousel-control-prev shadow rounded" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
