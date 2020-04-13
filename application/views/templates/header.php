5305a2bc6010d0347622c7000b0804dd5a2b9a52
jcmanhulad <57318349+jcmanhulad@users.noreply.github.com>
2020-04-13 15:11:59 +0800

    estimate meanu

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -29,7 +29,10 @@
               <li class="nav-item">
                 <a class="nav-link" href="<?php echo base_url(); ?>faq">FAQs</a>
               </li>
-              <?php if($this->session->userdata('logged_in')): ?>            
+              <li class="nav-item">
+                <a class="nav-link" href="<?php echo base_url(); ?>#">Estimate Rent</a>
+              </li>
+              <?php if($this->session->userdata('logged_in')): ?>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   Booking
