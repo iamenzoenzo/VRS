d90d88cac12ab7195154168373c06719361861fa
jcmanhulad <57318349+jcmanhulad@users.noreply.github.com>
2020-04-13 15:09:15 +0800

    disable menus if not logged in

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -29,6 +29,7 @@
               <li class="nav-item">
                 <a class="nav-link" href="<?php echo base_url(); ?>faq">FAQs</a>
               </li>
+              <?php if($this->session->userdata('logged_in')): ?>            
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   Booking
@@ -59,6 +60,7 @@
                   <a class="dropdown-item" href="<?php echo base_url(); ?>status/index">View Status</a>
                 </div>
               </li>
+              <?php endif; ?>
             </ul>
           </div>
         </nav>
