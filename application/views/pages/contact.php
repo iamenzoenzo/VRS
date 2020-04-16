9aec9b59cad68b99df771b0ab0ddbacf86d095a7
Enzo Enzo <57203957+enzoupou@users.noreply.github.com>
2020-04-16 12:26:16 +0800

    Responsive navbar #9

--- application/views/pages/contact.php
+++ application/views/pages/contact.php
@@ -1,7 +1,8 @@
 <title>Contact Us</title>
 <h2>Contact Us</h2>
 <p>We'd love to hear from you!</p>
-<div class="card">
+<div class="card-deck">
+  <div class="card">
   <div class="card-body">
     <h4 class="card-title">Address</h4>
     <p class="card-text"><?php echo $address['value'];?></p>
@@ -12,4 +13,15 @@
     <h4 class="card-title">Email</h4>
     <a target="_top" class="card-link"><?php echo $email['value'];?></a>
   </div>
+</div>
+<div class="card">
+  <div class="card-body">
+    <!--Google map-->
+		<div id="map-container-google-1" class="z-depth-1-half map-container embed-responsive embed-responsive-16by9" style="height: 500px">
+  		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63236.43525748331!2d125.01711575747619!3d7.866033366416994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32ff1bd874c5d051%3A0x9255561c02d41de0!2sHagkol!5e0!3m2!1sen!2sph!4v1587009926462!5m2!1sen!2sph" class="embed-responsive-item" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+		</div>
+
+		<!--Google Maps-->
+  </div>
+</div>
 </div>
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,11 +1,63 @@
 
 </div> <!-- end of container-fluid located in navbar -->
-<div class="fixed-bottom mt-3 bg-dark border-top box-shadow text-center">
-<footer class="footer py-3 ">
-  <div class="container">
-    <span class="text-muted">Copyright &copy; 2020</span>
-  </div>
-</footer>
+<div class="container">
+  <br>
+  <br>
+  <hr>
+  <br>
+  <br>
 </div>
+<div class="jumbotron bg-light" style="margin-bottom:0">
+   <div class="container">
+   <hr>
+   <a class="nav-link text-light" href="<?php echo base_url(); ?>"><img src="<?php echo base_url();?>assets/images/system_images/vrslogo.png" width="30" height="30">&nbsp;VRS</a>
+
+   <div class="card-columns bg-default">
+    <div class="card">
+      <div class="card-body">
+        <small>
+        <p><a class="nav-link" href="<?php echo base_url(); ?>about">About Us</a>
+        <a class="nav-link" href="<?php echo base_url(); ?>contact">Contact Us</a>
+        <a class="nav-link" href="<?php echo base_url(); ?>faq">FAQs</a></p>
+      </small>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <!-- Add font awesome icons -->
+        <small>
+        <p>
+        <a class="nav-link" href="#"><i class="fa fa-facebook"></i> facebook</a>
+        <a class="nav-link" href="#"><i class="fa fa-twitter"></i> twitter</a>
+        <a class="nav-link" href="#"><i class="fa fa-youtube"></i> youtube</a>
+        </p>
+      </small>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-body">
+        <!-- Add font awesome icons -->
+        <small>
+        <p>
+
+        </p>
+      </small>
+      </div>
+    </div>
+
+  </div>
+
+
+  </div>
+</div>
+
+
+
+<div class="jumbotron text-center bg-dark text-light" style="margin-bottom:0">
+  <small><p>Copyright &copy; 2020 VRS Inc. All rights reserved.
+    <a href="<?php echo base_url(); ?>privacy">Privacy Policy</a> | <a href="<?php echo base_url(); ?>terms">Terms of Use</a></p>
+  </small>
+</div>
+
 </body>
 </html>
