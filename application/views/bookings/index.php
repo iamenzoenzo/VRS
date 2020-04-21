95970f9291d115b79760711bdd71193562952029
jcmanhulad <57318349+jcmanhulad@users.noreply.github.com>
2020-04-22 01:05:44 +0800

    booking update

--- application/views/bookings/index.php
+++ application/views/bookings/index.php
@@ -23,24 +23,29 @@
   <thead>
     <tr>
       <th scope="col">#</th>
+      <th scope="col-4">Reference#</th>
       <th scope="col-4">Client</th>
       <th scope="col-4">Vehicle</th>
       <th scope="col">Start Date</th>
       <th scope="col">End Date</th>
       <th scope="col"># of days</th>
+      <th scope="col">Total Rent</th>
       <th scope="col">Status</th>
       <th scope="col">Actions</th>
     </tr>
   </thead>
   <tbody>
     <?php $counter=0; foreach ($bookings as $booking):?>
-        <tr class='clickable-row' title="Click to view this booking" data-href="<?php echo base_url().'bookings/view/'.$booking['BookingId'];?>">
+        <!--tr class='clickable-row' title="Click to view this booking" data-href="<?php echo base_url().'bookings/view/'.$booking['BookingId'];?>" -->
+        <tr>
         <td><?php $counter++; echo $counter ;?></td>
+        <td><?php echo $booking['reference_number'] ;?></td>
         <td><?php echo '<b>'.$booking['name'].'</b></br><small>'.$booking['email_address'].'</br>#'.$booking['contact_number'].'</br>'.$booking['address'].'</small>' ;?></td>
-        <td><?php echo $booking['code_name'].' ('.$booking['plate_number'].')' ;?></td>
+        <td><?php echo '<b>'.$booking['code_name'].'</b></br><small>Plate#: '.$booking['plate_number'].'</br>Rent per day: ₱'.number_format($booking['RentPerDay'],2).'</small>' ;?></td>
         <td><?php echo $booking['start_date'] ;?></td>
         <td><?php echo $booking['end_date'] ;?></td>
         <td><?php echo $booking['number_of_days'] ;?></td>
+        <td><?php echo '₱'.number_format(($booking['number_of_days'] * $booking['driver_fee_current'])+($booking['number_of_days'] * $booking['rental_fee_current'])-($booking['rental_discount']),2) ;?></td>
         <td class="bg-<?php echo $booking['bootstrap_bg_color'];?>"><?php echo $booking['label'] ;?></td>
         <td>
           <a class="btn btn-info" href="<?php echo base_url().'bookings/view/'.$booking['BookingId'];?>">View</a>
@@ -49,7 +54,7 @@
     <?php endforeach;?>
 
     <?php if($counter==0):?>
-      <tr><td colspan="8">No data to show</td></tr>
+      <tr><td colspan="9">No data to show</td></tr>
     <?php endif;?>
   </tbody>
 </table>
